123842399a6ce0a453ba3fc074e7bb65325bda5c
alexeysp11 <alexeyspiridon1111@gmail.com>
2020-09-04 19:34:52 +0500

    UI drafts

--- flaskblog.py
+++ flaskblog.py
@@ -26,11 +26,35 @@ posts = [
 @app.route("/")
 @app.route("/home")
 def home():
-    return render_template('home.html', posts=posts)
+    return render_template('home.html')
 
 @app.route("/about")
 def about():
     return render_template('about.html')
+
+@app.route("/login")
+def login():
+    return render_template('login.html')
+
+@app.route("/profile")
+def profile():
+    return render_template('profile.html', posts=posts)
+
+@app.route("/feed")
+def feed():
+    return render_template('feed.html', posts=posts)
+
+@app.route("/register")
+def register():
+    return render_template('register.html')
+
+@app.route("/forgot_password")
+def forgot_password():
+    return render_template('forgot_password.html')
+
+@app.route("/new")
+def new():
+    return render_template('new.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
