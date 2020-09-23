ebec1828e79f86779cdc7929c9fd9ca965bc846e
alexeysp11 <alexeyspiridon1111@gmail.com>
2020-09-23 22:52:47 +0500

    Added errorhandler(404)

--- flaskblog.py
+++ flaskblog.py
@@ -87,5 +87,10 @@ def new():
     return render_template('new.html', form=form)
 
 
+@app.errorhandler(404)
+def error404(e):
+    return render_template('error404.html')
+
+
 if __name__ == '__main__':
     app.run(debug=True)
